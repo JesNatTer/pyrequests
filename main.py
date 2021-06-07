f1a6f213c8a341ed3ddee5f70c4bba93216f85dd
JesNatTer <jessenterblanche@gmail.com>
2021-06-07 12:21:39 +0200

    update

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from tkinter import *
 import tkinter.font as tfont
+from tkinter import messagebox
 
 # creates window of program
 root = Tk()
@@ -25,26 +26,29 @@ cityentry.place(x=140, y=60)
 #  fetches and displays relevant weather data in labels
 def getdata():
     city = cityentry.get()
-    response = requests.get(
-        "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0bcce0200aa079df12eb18d5a972d541")
-    data = response.json()
-    temp = int(data['main']['temp'] - 273.15)
-    templ = Label(root, text=str(temp) + '°C', font=font1, fg='white', bg='#222')
-    templ.place(x=140, y=150)
-    tempmin = int(data['main']['temp_min'] - 273.15)
-    tempminl = Label(root, text='Min:'+str(tempmin)+'°C', font=font4, fg='white', bg='#222')
-    tempminl.place(x=135, y=200)
-    tempmax = int(data['main']['temp_max'] - 273.15)
-    tempmaxl = Label(root, text='Max: '+str(tempmax)+'°C', font=font4, fg='white', bg='#222')
-    tempmaxl.place(x=200, y=200)
-    cover = Label(root, text=str(data['weather'][0]['main']),width=20, font=font3, fg='white', bg='#222', justify='center')
-    cover.place(x=90, y=230)
-    cloud = Label(root, text='Cloud Cover: '+ str(data['clouds']['all']) + '%', font=font3, fg='white', bg='#222')
-    cloud.place(x=125, y=290)
-    humidity = Label(root, text='Humidity: ' + str(data['main']['humidity']) + '%', font=font3, fg='white', bg='#222')
-    humidity.place(x=135, y=330)
-    wind = Label(root, text='Wind Speed: ' + str(data['wind']['speed']) + 'km/h', font=font3, fg='white', bg='#222')
-    wind.place(x=99, y=370)
+    try:
+        response = requests.get(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=0bcce0200aa079df12eb18d5a972d541")
+        data = response.json()
+        temp = int(data['main']['temp'] - 273.15)
+        templ = Label(root, text=str(temp) + '°C', font=font1, fg='white', bg='#222')
+        templ.place(x=140, y=150)
+        tempmin = int(data['main']['temp_min'] - 273.15)
+        tempminl = Label(root, text='Min:'+str(tempmin)+'°C', font=font4, fg='white', bg='#222')
+        tempminl.place(x=135, y=200)
+        tempmax = int(data['main']['temp_max'] - 273.15)
+        tempmaxl = Label(root, text='Max: '+str(tempmax)+'°C', font=font4, fg='white', bg='#222')
+        tempmaxl.place(x=200, y=200)
+        cover = Label(root, text=str(data['weather'][0]['main']),width=20, font=font3, fg='white', bg='#222', justify='center')
+        cover.place(x=90, y=230)
+        cloud = Label(root, text='Cloud Cover: '+ str(data['clouds']['all']) + '%', font=font3, fg='white', bg='#222')
+        cloud.place(x=125, y=290)
+        humidity = Label(root, text='Humidity: ' + str(data['main']['humidity']) + '%', font=font3, fg='white', bg='#222')
+        humidity.place(x=135, y=330)
+        wind = Label(root, text='Wind Speed: ' + str(data['wind']['speed']) + ' km/h', font=font3, fg='white', bg='#222')
+        wind.place(x=99, y=370)
+    except ValueError:
+        messagebox.showerror("Invalid Input", "Please enter a name of a city.")
 
 
 #  button for function 'getdata'
